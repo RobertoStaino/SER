61b34ac7f8256f291675f4b05d276a4b64dbb3fb
roberto.staino <roberto.staino97@gmail.com>
2023-06-27 17:55:50 +0200

    Change name repo and small fix

--- utils/audio_preprocess.py
+++ utils/audio_preprocess.py
@@ -64,3 +64,18 @@ def audio_section(Xdb, sr = 22050, window = 0.5, threshold = -40): # works only 
     #     audio_sections.append(x[start_time:end_time])
 
     return unify_intervals(speech_sections) 
+
+def noise(data):
+    noise_amp = 0.035*np.random.uniform()*np.amax(data)
+    data = data + noise_amp*np.random.normal(size=data.shape[0])
+    return data
+
+def stretch(data, rate=0.8):
+    return librosa.effects.time_stretch(data, rate)
+
+def shift(data):
+    shift_range = int(np.random.uniform(low=-5, high = 5)*1000)
+    return np.roll(data, shift_range)
+
+def pitch(data, sampling_rate, pitch_factor=0.7):
+    return librosa.effects.pitch_shift(data, sampling_rate, pitch_factor)
