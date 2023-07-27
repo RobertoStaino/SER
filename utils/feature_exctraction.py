2107393754e299275565e3b784e0a6fe91f83106
roberto.staino <roberto.staino97@gmail.com>
2023-07-27 17:12:59 +0200

    added more models for benchmark and exploration. spectr: pt1

--- utils/feature_exctraction.py
+++ utils/feature_exctraction.py
@@ -2,49 +2,56 @@ import librosa
 import numpy as np
 from utils.audio_preprocess import random_augmentation
 
-def extract_features(data, sample_rate = 22050, n_mfcc = 13):
-    # ZCR
-    result = np.array([])
-    zcr = librosa.feature.zero_crossing_rate(y=data)
-    result = np.hstack((result, np.mean(zcr, axis=1))) # MEAN - stacking horizontally
-    result = np.hstack((result, np.std(zcr, axis=1))) # STD - stacking horizontally
+def extract_features(data, sample_rate = 22050, n_mfcc = 13, spect = False):
+    if (spect == False):
+        # ZCR
+        result = np.array([])
+        zcr = librosa.feature.zero_crossing_rate(y=data)
+        result = np.hstack((result, np.mean(zcr, axis=1))) # MEAN - stacking horizontally
+        result = np.hstack((result, np.std(zcr, axis=1))) # STD - stacking horizontally
 
-    # Chroma_stft
-    stft = np.abs(librosa.stft(data))
-    chroma_stft = librosa.feature.chroma_stft(S=stft, sr=sample_rate)
-    result = np.hstack((result, np.mean(chroma_stft, axis = 1))) # MEAN - stacking horizontally
-    result = np.hstack((result, np.std(chroma_stft, axis=1))) # STD - stacking horizontally
+        # Chroma_stft
+        #stft = np.abs(librosa.stft(data))
+        #chroma_stft = librosa.feature.chroma_stft(S=stft, sr=sample_rate)
+        #result = np.hstack((result, np.mean(chroma_stft, axis = 1))) # MEAN - stacking horizontally
+        #result = np.hstack((result, np.std(chroma_stft, axis=1))) # STD - stacking horizontally
 
-    # MFCC
-    mfcc = librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc = n_mfcc)
-    result = np.hstack((result, np.mean(mfcc, axis=1))) # MEAN - stacking horizontally
-    result = np.hstack((result, np.std(mfcc, axis=1))) # STD - stacking horizontally
+        # MFCC
+        mfcc = librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc = n_mfcc)
+        result = np.hstack((result, np.mean(mfcc, axis=1))) # MEAN - stacking horizontally
+        result = np.hstack((result, np.std(mfcc, axis=1))) # STD - stacking horizontally
 
-    # spectral centroids 
-    x_spectral_centroid = librosa.feature.spectral_centroid(y=data, sr=sample_rate)
-    result = np.hstack((result, x_spectral_centroid.mean())) # MEAN - stacking horizontally
-    result = np.hstack((result, x_spectral_centroid.std())) # STD - stacking horizontally
-    x_spectral_bw = librosa.feature.spectral_bandwidth(y=data, sr=sample_rate)
-    result = np.hstack((result, x_spectral_bw.mean())) # MEAN - tacking horizontally
-    result = np.hstack((result, x_spectral_bw.std())) # STD - stacking horizontally
+        # spectral centroids 
+        x_spectral_centroid = librosa.feature.spectral_centroid(y=data, sr=sample_rate)
+        result = np.hstack((result, x_spectral_centroid.mean())) # MEAN - stacking horizontally
+        result = np.hstack((result, x_spectral_centroid.std())) # STD - stacking horizontally
+        x_spectral_bw = librosa.feature.spectral_bandwidth(y=data, sr=sample_rate)
+        result = np.hstack((result, x_spectral_bw.mean())) # MEAN - tacking horizontally
+        result = np.hstack((result, x_spectral_bw.std())) # STD - stacking horizontally
 
-    # Root Mean Square Value
-    rms = librosa.feature.rms(y=data)
-    result = np.hstack((result, np.mean(rms,  axis=1))) # MEAN - stacking horizontally
-    result = np.hstack((result, np.std(rms, axis=1))) # STD - stacking horizontally
+        # Root Mean Square Value
+        rms = librosa.feature.rms(y=data)
+        result = np.hstack((result, np.mean(rms,  axis=1))) # MEAN - stacking horizontally
+        result = np.hstack((result, np.std(rms, axis=1))) # STD - stacking horizontally
 
-    # MelSpectogram
-    mel = librosa.feature.melspectrogram(y=data, sr=sample_rate)
-    #result = np.hstack((result, np.mean(mel,  axis=1))) # MEAN - stacking horizontally
-    result = np.hstack((result, np.std(mel, axis=1))) # STD - stacking horizontally
-    
+        # MelSpectogram
+        mel = librosa.feature.melspectrogram(y=data, sr=sample_rate)
+        #result = np.hstack((result, np.mean(mel,  axis=1))) # MEAN - stacking horizontally
+        result = np.hstack((result, np.std(mel, axis=1))) # STD - stacking horizontally
+
+    else:
+        # MelSpectogram
+        mel = librosa.feature.melspectrogram(y=data, sr=sample_rate)
+        #result = np.hstack((result, np.mean(mel,  axis=1))) # MEAN - stacking horizontally
+        result = np.hstack((result, np.array(mel, axis=1))) # STD - stacking horizontally
+
     return result
 
-def get_features(path, synth = True, n_iter = 1):
+def get_features(path, synth = True, n_iter = 1, spect = False):
     data, sample_rate = librosa.load(path) # , duration=2.5, offset=0.6
     
     # without augmentation
-    res1 = extract_features(data)
+    res1 = extract_features(data, spect = False)
 
     if (synth):
         result = np.array(res1)
@@ -52,7 +59,7 @@ def get_features(path, synth = True, n_iter = 1):
         while counter < n_iter:
             # data with random augmentation
             noise_data = random_augmentation(data, sample_rate)
-            res2 = extract_features(noise_data)
+            res2 = extract_features(noise_data, spect = False)
             result = np.vstack((result, res2)) # stacking vertically
             counter += 1
     else:
