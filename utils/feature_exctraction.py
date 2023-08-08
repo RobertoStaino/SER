30e46a7832b12363c1c8f157d19d10dec6fed053
roberto.staino <roberto.staino97@gmail.com>
2023-08-08 16:46:49 +0200

    Exctraction function TODO: 2d spect (maybe)

--- utils/feature_exctraction.py
+++ utils/feature_exctraction.py
@@ -1,9 +1,12 @@
 import librosa
 import numpy as np
+import pandas as pd
 from utils.audio_preprocess import random_augmentation
+from sklearn.preprocessing import OneHotEncoder, StandardScaler 
 
-def extract_features(data, sample_rate = 22050, n_mfcc = 13, spect = False):
-    if (spect == False):
+
+def extract_features(data, sample_rate = 22050, n_mfcc = 13, feats = False):
+    if feats:
         # ZCR
         result = np.array([])
         zcr = librosa.feature.zero_crossing_rate(y=data)
@@ -45,21 +48,21 @@ def extract_features(data, sample_rate = 22050, n_mfcc = 13, spect = False):
 
     return result
 
-def get_features(path, synth = True, n_iter = 1, spect = False, debug = False):
+def get_features(path, n_aug = 0, feats = False, debug = False):
     data, sample_rate = librosa.load(path) # , duration=2.5, offset=0.6
     
     # without augmentation
-    res1 = extract_features(data, spect = spect)
+    res1 = extract_features(data, feats = feats)
     if (debug == True):
         print("pre new axis:", res1.shape)
 
-    if (synth):
+    if (n_aug > 0):
         result = np.array(res1)
         counter = 0
-        while counter < n_iter:
+        while counter < n_aug:
             # data with random augmentation
             noise_data = random_augmentation(data, sample_rate)
-            res2 = extract_features(noise_data, spect = False)
+            res2 = extract_features(noise_data, feats = feats)
             result = np.vstack((result, res2)) # stacking vertically
             counter += 1
     else:
@@ -68,3 +71,50 @@ def get_features(path, synth = True, n_iter = 1, spect = False, debug = False):
             print("post new axis:", result.shape)
     
     return result
+
+def get2d_data (train, test, feats = False):
+    x_train, y_train = [], []
+    x_test, y_test = [], []
+
+    # TRAIN + Augmentation
+    for path, emotion in zip(train.Path, train.Emotions):
+        n_aug = np.random.randint(1, 4) if (str(np.random.randint(1, 10)) in path) else 0 #and np.random.choice([True, False])
+        feature = get_features(path, n_aug=n_aug, feats=feats)
+        for ele in feature:
+            x_train.append(ele)
+            y_train.append(emotion)
+    print("Train exctraction complete. x_train.shape ->", len(x_train))
+    
+    # TEST
+    for path, emotion in zip(test.Path, test.Emotions):
+        feature = get_features(path, feats=feats)
+        for ele in feature:
+            x_test.append(ele)
+            y_test.append(emotion)
+    print("Test exctraction complete. x_test.shape ->", len(x_test))
+    
+    # I want them as arrays
+    Matrix_train = pd.DataFrame(x_train)
+    Matrix_train['labels'] = y_train
+    Matrix_test = pd.DataFrame(x_test)
+    Matrix_test['labels'] = y_test
+    x_train = Matrix_train.iloc[: ,:-1].values
+    y_train = Matrix_train['labels'].values
+    x_test = Matrix_test.iloc[: ,:-1].values
+    y_test = Matrix_test['labels'].values
+    
+    # one hot encoding
+    encoder = OneHotEncoder()
+    y_train = encoder.fit_transform(np.array(y_train).reshape(-1,1)).toarray()
+    y_test = encoder.fit_transform(np.array(y_test).reshape(-1,1)).toarray()
+    
+    # standardScaler    
+    #scaler = StandardScaler()
+    #for i in range(x_train.shape[0]):
+    #    x_train[i] = scaler.fit_transform(x_train[i])
+
+
+    #for i in range(x_test.shape[0]):
+    #    x_test[i] = scaler.fit_transform(x_test[i])
+            
+    return x_train, y_train, x_test, y_test
--- utils/feature_exctraction_v2.py
+++ utils/feature_exctraction_v2.py
@@ -1,9 +1,10 @@
 import librosa
 import numpy as np
 from utils.audio_preprocess import random_augmentation
+from sklearn.preprocessing import OneHotEncoder
 
 
-def extract_features(data, sample_rate=22050, n_mfcc=13, feats=True):
+def extract_3dfeatures(data, sample_rate=22050, n_mfcc=13, feats=True):
     if feats:
         # ZCR
         zcr = librosa.feature.zero_crossing_rate(y=data)
@@ -36,21 +37,74 @@ def extract_features(data, sample_rate=22050, n_mfcc=13, feats=True):
     return result
 
 
-def get_features(path, n_aug=0, feats=True):
+def get_3dfeatures(path, n_aug=0, feats=True):
     data, sample_rate = librosa.load(path)  # , duration=2.5, offset=0.6
 
     # without augmentation
-    res1 = extract_features(data, feats=feats)
+    res1 = extract_3dfeatures(data, feats=feats)
     result = np.array(res1)[np.newaxis]
 
     synth_results = list()
-    if n_aug:
+    if n_aug != 0:
         synth_results.append(result[0])
         for _ in range(n_aug):
             # data with random augmentation
             noise_data = random_augmentation(data, sample_rate)
-            res2 = extract_features(noise_data, feats=feats)
+            res2 = extract_3dfeatures(noise_data, feats=feats)
             synth_results.append(res2)
         result = synth_results
 
     return result
+
+
+def get3d_data (train, test, feats = False): # adjust n_aug for the number of synth data
+    x_train, y_train = [], []
+    x_test, y_test = [], []
+
+    # TRAIN + Augmentation
+    for path, emotion in zip(train.Path, train.Emotions):
+        n_aug = np.random.randint(1, 4) if (str(np.random.randint(1, 10)) in path) else 0 #and np.random.choice([True, False])
+        feature = get_3dfeatures(path, n_aug=n_aug, feats=feats)
+        for ele in feature:
+            x_train.append(ele)
+            y_train.append(emotion)
+    print("Train exctraction complete. x_train ->", len(x_train))
+    
+    # TEST
+    for path, emotion in zip(test.Path, test.Emotions):
+        feature = get_3dfeatures(path, feats=feats)
+        for ele in feature:
+            x_test.append(ele)
+            y_test.append(emotion)
+    print("Test exctraction complete. x_test ->", len(x_test))
+
+    print("Final processing for NN.")
+    # Match rows and columns, because the time of the audios are different
+    max_rows, max_cols = 0,0
+    for atens in [x_train, x_test]: # Find the maximum number of rows and columns among all arrays
+        max_rows = max(max_rows, max(a.shape[0] for a in atens))
+        max_cols = max(max_cols, max(a.shape[1] for a in atens))
+        
+    for atens in [x_train, x_test]:
+        for i, a in enumerate(atens): # Pad the smaller arrays with zeros to match the maximum shape
+            atens[i] = np.pad(a, ((0, max_rows - a.shape[0]), (0, max_cols - a.shape[1])), 'constant')
+
+    # Stack the padded arrays using numpy.stack()
+    x_train = np.stack(x_train)
+    x_test = np.stack(x_test)
+    
+    # one hot encoding
+    encoder = OneHotEncoder()
+    y_train = encoder.fit_transform(np.array(y_train).reshape(-1,1)).toarray()
+    y_test = encoder.fit_transform(np.array(y_test).reshape(-1,1)).toarray()
+    
+    # making our data compatible to model.
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test =  np.expand_dims(x_test, axis=-1)
+    print("x_train.shape -> ", x_train.shape)
+    print("y_train.shape -> ", y_train.shape)
+    print("x_test.shape -> ", x_test.shape)
+    print("y_test.shape -> ", y_test.shape)
+    print("Done.")
+    
+    return x_train, y_train, x_test, y_test
